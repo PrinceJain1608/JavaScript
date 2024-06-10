6d4ba3e6ce909bee9d385e644f12cb0564807aa3
Prince Jain <138566479+PrinceJain1608@users.noreply.github.com>
2024-06-10 12:51:59 +0000

    Variables

--- basics/variables.js
+++ basics/variables.js
@@ -3,8 +3,9 @@ let name="Prince Jain";
 var age=19;    //dont use it
 //id=345;    not allowed
 let phone;
+city="Agra";
 name="Devendra Kumar Jain";
 // console.log(name);
 age=20;
 // console.log(age);
-console.table([id,name,age,phone]);   //output in the form of table
+console.table([id,name,age,phone,city]);   //output in the form of table
