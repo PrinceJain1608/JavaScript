b3041e8a00194de21763bb18f97209f7dc7deb1b
Prince Jain <138566479+PrinceJain1608@users.noreply.github.com>
2024-06-10 13:08:25 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,18 @@
+let name="Prince";
+let age=19;
+let city="Agra";
+
+/*
+//datatypes
+Numbers=> range from  2 to the power 53
+bigint-for large Numbers
+string=""
+Boolean-true/false
+null-standalone value
+undefined-
+Symbol-unique
+*/
+console.log(typeof "hello");
+console.log(typeof null);       //object
+console.log(typeof undefined);   //undefined
+console.log(typeof true);
