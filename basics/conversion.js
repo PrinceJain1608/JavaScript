d299aad3cf2ee60c500d7646da31e248e8f61695
Prince Jain <138566479+PrinceJain1608@users.noreply.github.com>
2024-06-13 06:40:21 +0000

    Conversion of data types

---
+++ basics/conversion.js
@@ -0,0 +1,12 @@
+let age=19;
+console.log(typeof age);
+
+let a="333aaa";
+console.log((typeof a));
+let b=Number(a);
+console.log((typeof b));
+console.log(b);   //NaN   not a number
+
+let c=null;
+console.log(typeof(c));   //object
+console.log(Number(c));    //0
