2a643c20d855107a85191ec6e3821c9fab595e8f
Prince Jain <138566479+PrinceJain1608@users.noreply.github.com>
2024-06-17 15:39:14 +0000

    for loop

---
+++ loops/for.js
@@ -0,0 +1,16 @@
+for(let i=1;i<=10;i++){
+    console.log(i);
+}
+
+for(let i=1;i<20;i++){
+    if(i==5){
+        break
+    }
+    console.log(i);
+}
+for(let i=1;i<20;i++){
+    if(i==5){
+        continue
+    }
+    console.log(i);
+}
